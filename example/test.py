f782cb5dd7a52ef74c68ee17b704f360db23402a
justin.zm <3907721@qq.com>
2020-05-02 21:46:24 +0800

    Initial commit

---
+++ example/test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/5/2 21:20
+# @Author  : justin.郑 3907721@qq.com
+# @File    : test.py.py
+# @Desc    :
+
+import web_extractor as we
+
+def test():
+    tmp = we.cs_test()
+    print(tmp)
+
+if __name__ == "__main__":
+    test()
